9fd9ee093d83cca5b995890423868fd63a6464ed
Patrick César <patrickcesar@MacBook-Pro-de-Patrick-Cesar.local>
2021-01-01 14:21:21 -0300

    change about me

--- src/pages/sobremim.tsx
+++ src/pages/sobremim.tsx
@@ -9,7 +9,13 @@ export default function NotFoundPage(): React.ReactElement {
     <Container>
       <Metatags title="Sobre mim" />
       <PageTitle>Sobre mim</PageTitle>
-      <p>Hey, meu nome é Patrick César e, além de Analista Computacional, sou estudante de desenvolvimento web.</p>
+      <p>Hey, meu nome é Patrick César se você chegou até aqui é porque você, assim como eu, é um fã de tecnologia.
+        Tenho 28 anos e o que mais fiz até agora foi tentar encontrar o caminho para algo que me faça feliz. 
+        Sou formado em Administração, professor particular de Inglês, Analista de HelpDesk e estudante de programação.
+        Sou baiano, com coração europeu (rs amo frio).
+        Sou um cinéfilo, leitor, e até fitness.
+        Você vai descobrir mais sobre esse ser através dos meus posts no blog!
+        Abraçossss.</p>
       <img src="/static/01.jpg" />
     </Container>
   );
