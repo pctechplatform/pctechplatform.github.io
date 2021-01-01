36ac1fb865eabf8a9eaf55ecb5b7dacb3ff70305
ptrick.cesar <patrick.cesars21@gmail.com>
2021-01-01 17:46:56 -0300

    changes aboutme

--- src/pages/sobremim.tsx
+++ src/pages/sobremim.tsx
@@ -16,14 +16,14 @@ export default function NotFoundPage(): React.ReactElement {
     <Container>
       <Metatags title="Sobre mim" />
       <PageTitle>Sobre mim</PageTitle>
+      <Imagem src="/01.jpg" />
       <p>
-        Hey, meu nome é Patrick César se você chegou até aqui é porque você, assim como eu, é um fã de tecnologia. Tenho
-        <p> 28 anos e o que mais fiz até agora foi tentar encontrar o caminho para algo que me faça feliz.</p>
+        Hey, meu nome é Patrick César se você chegou até aqui é porque você, assim como eu, é um fã de tecnologia.
+        <p> Tenho 28 anos e o que mais fiz até agora foi tentar encontrar o caminho para algo que me faça feliz.</p>
         Sou formado em Administração, professor particular de Inglês, Analista de HelpDesk e estudante de programação.
         Sou baiano, com coração europeu (rs amo frio). Sou um cinéfilo, leitor, e até fitness. Você vai descobrir mais
         sobre esse ser através dos meus posts no blog! Abraçossss.
       </p>
-      <Imagem src="/01.jpg" />
     </Container>
   );
 }
